69a100821ef28024811f4af650eec403a32c798e
RiyaPatel2002 <03112002riyapatel@gmail.com>
2025-04-23 12:40:42 +0530

    create a customer list page

---
+++ resources/js/components/CreateOrEditCustomer.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+</script>
+<template>
+    <div class="mx-auto max-w-7xl w-full">
+        <!-- add customer details -->
+        <div class="mt-1">
+            <Icon icon="material-symbols:add-rounded" width="30" height="30" />
+        </div>
+    </div>
+</template>
