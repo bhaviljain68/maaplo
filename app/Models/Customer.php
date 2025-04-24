5ec599b16f62beb2f1687b04e6a8d33f630e55bb
Bhavil Jain <bhaviljain68@gmail.com>
2025-04-24 11:35:38 +0530

    Implemented soft deletes, relations between Customer and Customer photos

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
-    use HasFactory;
+    use SoftDeletes;
 
     protected $table = 'customers';
 
@@ -21,8 +22,46 @@ class Customer extends Model
         'address',
         'notes',
     ];
-    public function setNameAttribute($value)
+
+    protected function name(): Attribute
     {
-        $this->attributes['name'] = ucfirst($value);
+        return Attribute::make(
+            get: fn(string $value) => ucfirst($value),
+            set: fn(string $value) => strtolower($value),
+        );
+    }
+
+    protected function gender(): Attribute
+    {
+        return Attribute::make(
+            get: function ($value) {
+                $return = "";
+                if ($value === 'm') $return = "Male";
+                if ($value === 'f') $return = "Female";
+                if ($value === 'o') $return = "Other";
+                return $return;
+            }
+        );
+    }
+
+//    Relations
+    public function photos()
+    {
+        $this->hasMany(CustomerPhoto::class, 'customer_id', 'id');
+    }
+
+    protected static function booted()
+    {
+        // When a Project is soft-deleted
+        static::deleting(function ($customer): void {
+            if (!$customer->isForceDeleting()) {
+                $customer->photos()->delete();
+            }
+        });
+
+        // When a Project is restored
+        static::restoring(function ($customer): void {
+            $customer->photos()->withTrashed()->restore();
+        });
     }
 }
