3afd0d4fe0c39a0dc77d2ba5723f126ba877727b
Goswami Sahil <goswamisahil1911@gmail.com>
2025-04-28 11:27:05 +0530

    change in user model for email_verified_at not work,remove extra softDeletes field

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,7 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => Carbon::parse($this->email_verified_at)->format("DD M YYYY HH:mm:ss"),
+            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
--- database/migrations/2025_04_18_082049_create_customer_photos_table.php
+++ database/migrations/2025_04_18_082049_create_customer_photos_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('label');
             $table->softDeletes();
             $table->timestamps();
-            $table->softDeletes();
         });
 
     }
