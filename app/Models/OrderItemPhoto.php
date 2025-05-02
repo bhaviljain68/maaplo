71b91f38a9320d6286fd021c420889d2c599eed6
richa2412 <70392225+richa2412@users.noreply.github.com>
2025-05-02 11:58:23 +0530

    OrderItemPhoto Model

--- app/Models/OrderItemPhoto.php
+++ app/Models/OrderItemPhoto.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderItemPhoto extends Model
 {
-    //
+    protected $table = 'order_item_photos';
+
+    Use SoftDeletes;
+
+    protected $fillable = [
+        'image_url',
+        'label',
+    ];
+
+    // Relationships
+
+    /**
+     * Get the order item that owns the photo.
+     */
+    public function orderItem()
+    {
+        return $this->belongsTo(OrderItem::class);
+    }   
 }
